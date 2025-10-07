4dc2da47871be721f60a2de45f185bd38c16a96d
Nasccped <pedro.main.tec@gmail.com>
2025-10-07 10:40:14 -0300

    chore: add problem 14 solve (rust)

---
+++ problems/00014-longest-common-prefix/rust/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code)]
+struct Solution {}
+
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut prefix: String = strs
+            .first()
+            .expect("`strs` len is always greater than 0")
+            .into();
+        for s in strs.into_iter().skip(1) {
+            while !s.starts_with(&prefix) {
+                let _ = prefix.pop();
+                if prefix.is_empty() {
+                    return prefix;
+                }
+            }
+        }
+        prefix
+    }
+}
