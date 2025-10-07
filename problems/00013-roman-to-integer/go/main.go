784fe8a338c5a81bad55c9d89a976b2085728e99
Nasccped <pedro.main.tec@gmail.com>
2025-10-07 10:16:40 -0300

    chore: add problem 13 solve (go)

---
+++ problems/00013-roman-to-integer/go/main.go
@@ -0,0 +1,14 @@
+package main
+
+func romanToInt(s string) int {
+	romans := map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	accum := 0
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && romans[s[i]] < romans[s[i+1]] {
+			accum -= romans[s[i]]
+		} else {
+			accum += romans[s[i]]
+		}
+	}
+	return accum
+}
